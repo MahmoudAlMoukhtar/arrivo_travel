348febb851f60890dcdf8e71d70671205fc0fe13
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-06-21 19:08:05 +0300

    import animat.css in _app.js,in _document.js try to add some links but not working

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import { Toaster } from "react-hot-toast";
-import { OurContextProvider } from "../context/OurContext";
+import {Toaster} from "react-hot-toast";
+import {OurContextProvider} from "../context/OurContext";
 import "../styles/globals.css";
+import "animate.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   return (
     <OurContextProvider>
       <Component {...pageProps} />
