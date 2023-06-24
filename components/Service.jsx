f653ddf5f8c177a7d2ee3fab2ab032a1d54cf28a
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-06-24 17:39:20 +0300

    fix responsaive style for cancel ttripe modal

--- components/Service.jsx
+++ components/Service.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import {useEffect} from "react";
+import {motion} from "framer-motion";
+import PrevTitle from "./PrevTitle";
 const Service = ({
   service,
   children,
@@ -9,7 +11,32 @@ const Service = ({
   title,
   description,
   imageSrc,
+  url,
 }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      const element = document.querySelector(".service_image");
+      if (element) {
+        element.classList.add("animate__animated", "animate__bounceOutLeft");
+      }
+    }, 500);
+  });
+  const variants = {
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        type: "spring",
+        stiffness: 200,
+        damping: 20,
+        duration: 0.5,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      scale: 0.5,
+    },
+  };
   return (
     <div
       className={`flex items-center justify-between flex-col-reverse  ${
@@ -18,7 +45,7 @@ const Service = ({
     >
       <div className="flex flex-col items-start  gap-[24px]">
         <div className="flex flex-col gap-[4px]">
-          <p className="text-orange font-bold-500 text-[16px]">{preTitle}</p>
+          <PrevTitle prevTitle={preTitle} />
           <h2 className=" text-[24px] sm:text-[28px] md:text-[36px]  font-bold-600">
             {title}
           </h2>
@@ -28,7 +55,7 @@ const Service = ({
           {children}
           {
             <Link
-              href="/travels-programs?type=programs"
+              href={`${url}`}
               className="bg-[#F08631] w-full md:w-[154px] py-[14px] px-[20px] text-white rounded-[8px] text-center"
             >
               تصفح الحجوزات
@@ -36,7 +63,12 @@ const Service = ({
           }
         </div>
       </div>
-      <div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={variants}
+        className="animate__zoomIn"
+      >
         <Image
           alt=""
           width={590}
@@ -44,7 +76,7 @@ const Service = ({
           className="w-[400px] lg:w-[590px] hidden sm:block"
           src={imageSrc}
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
