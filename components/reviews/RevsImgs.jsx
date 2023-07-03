63605a8840d51a4ea039662fbcd01ca43c5743f6
Mahmoud Al-Moukhtar <101312716+MahmoudAlMoukhtar@users.noreply.github.com>
2023-07-03 22:02:25 +0300

    reduce daly for images animations

--- components/reviews/RevsImgs.jsx
+++ components/reviews/RevsImgs.jsx
@@ -8,7 +8,7 @@ const RevsImgs = () => {
     show: {
       translateY: 0,
       opacity: 1,
-      transition: {duration: 0.7, delay: 0.4},
+      transition: {duration: 0.7, delay: 0.2},
     },
   };
   return (
@@ -34,7 +34,7 @@ const RevsImgs = () => {
           whileInView={{
             translateY: 0,
             opacity: 1,
-            transition: {duration: 1, delay: 0.5},
+            transition: {duration: 1, delay: 0.3},
           }}
           whileHover={{x: -20, y: -20}}
           className="relative w-[141.85px] h-[183.38px] md:h-[173.52px] md:w-[134.22px] lg:h-[284.53px] lg:w-[220.09px]"
@@ -54,7 +54,7 @@ const RevsImgs = () => {
           whileInView={{
             translateY: 0,
             opacity: 1,
-            transition: {duration: 1, delay: 0.6},
+            transition: {duration: 1, delay: 0.4},
           }}
           whileHover={{x: +20, y: +10}}
           className="relative w-[90px] h-[98px] md:h-[92.5px] md:w-[134.22px] lg:h-[151.68px] lg:w-[220.09px]"
@@ -72,7 +72,7 @@ const RevsImgs = () => {
           whileInView={{
             translateY: 0,
             opacity: 1,
-            transition: {duration: 1, delay: 0.7},
+            transition: {duration: 1, delay: 0.5},
           }}
           whileHover={{x: +5, y: +20}}
           className="relative w-[141.85px] h-[183.38px] md:h-[173.52px] md:w-[134.22px] lg:h-[284.53px] lg:w-[220.09px]"
@@ -90,7 +90,7 @@ const RevsImgs = () => {
           whileInView={{
             x: 0,
             opacity: 1,
-            transition: {duration: 1, delay: 0.7},
+            transition: {duration: 1, delay: 0.6},
           }}
           whileHover={{x: -10, y: -20}}
           className="relative w-[90px] h-[98px] md:h-[92.5px] md:w-[134.22px] lg:h-[151.68px] lg:w-[220.09px]"
